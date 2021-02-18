5025722daa5eb2afbfcab07e316a58edb7823344
Ryan Soeyadi <rsoeyadi@me.com>
2021-02-18 13:10:39 -0500

    Moved parent directory inside to child

---
+++ Java Files/test.java
@@ -0,0 +1,17 @@
+public class test {
+
+
+	public static void main(String[ ] args) {
+		StringBuffer name = 	new StringBuffer("Sam");
+		boolean ok = false;
+		update(name, ok);
+		System.out.println(name + ", "  + ok);
+	}
+
+	public static void update(StringBuffer name, boolean ok) {
+		name.append(" Sultan");
+		ok = true;
+		
+		}
+
+}
